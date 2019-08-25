84b0a46f1af25ea5fd60de9e9c42bdd672cf1cc9
Saley SEINI <ssalifo@hotmail.fr>
2019-08-25 22:43:18 +0200

    Component Part1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,12 @@
+// import { SpawnSyncOptionsWithBufferEncoding } from "child_process";
+
+export class Dish {
+  id: string;
+  name: string;
+  image: string;
+  category: string;
+  featured: boolean;
+  label: string;
+  price: string;
+  description: string;
+}
